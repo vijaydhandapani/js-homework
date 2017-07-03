2f790ef64192fe9c2711a3403e1362ec107a73ef
vijaydhandapani <vijayakrishnaraj@gmail.com>
2017-07-03 00:18:40 -0400

    added test cases

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -10,7 +10,24 @@ describe('UserService', () => {
     });
   });
 
-  it('should ...', inject([UserService], (service: UserService) => {
+  it('should create the service object', inject([UserService], (service: UserService) => {
     expect(service).toBeTruthy();
   }));
+
+   it('should return the user object for the given id ', inject([UserService], (service: UserService) => {
+    let user = service.getUser(1);
+    expect(user).toBeTruthy();
+    expect(user.email).toEqual('fred.flintstone@slaterockandgravel.com')
+  }));
+  it('should return 3 users', inject([UserService], (service: UserService) => {
+    let users:Array<any> = service.getUsers();
+    expect(users.length).toEqual(3)
+  }));
+
+ /* it('should ...', inject([UserService], (service: UserService) => {
+    let user = service.createUser("vijay" ,"raj", "v@gmail.com");
+    expect(user.id).toEqual(4);
+    expect(user.firstName).toEqual('vijay');
+    
+  }));*/
 });
