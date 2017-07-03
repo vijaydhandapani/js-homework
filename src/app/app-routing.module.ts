cedb10f75e24b162dda0c476d80ed265de5ebaa3
vijaydhandapani <vijayakrishnaraj@gmail.com>
2017-07-03 16:54:38 -0400

    added create-user path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogoutComponent } from './account/logout.component';
 import { UserDetailComponent } from './user/detail.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 const mainRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'users/:id', component: UserDetailComponent }
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: 'create-user', component: CreateUserComponent }
+
 ];
 
 @NgModule({
